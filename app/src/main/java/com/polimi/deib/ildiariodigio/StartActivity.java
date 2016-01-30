5813e053ff8183d32d36b3162f98be91475f8655
Marc <marc@localhost.localdomain>
2016-01-30 19:18:02 +0100

    First commit

---
+++ app/src/main/java/com/polimi/deib/ildiariodigio/StartActivity.java
@@ -0,0 +1,19 @@
+package com.polimi.deib.ildiariodigio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageButton;
+
+public class StartActivity extends AppCompatActivity {
+
+    ImageButton enter_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        //enter_button =
+    }
+}
