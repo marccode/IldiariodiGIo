ba2d5d03e875c512c027fd084d2b3680c6113999
Diego <dialgu_zgz@hotmail.com>
2016-02-07 18:03:18 +0100

    login hecho, funciona correctamente
    lo del teclado no lo he conseguido hacer bien
    
    he creado firstlogin y modificaactivity
    ademas he modificado loginactivity y startactivity
    vamos todas las que habia hasta ahora jajajaja

--- app/src/main/java/com/polimi/deib/ildiariodigio/LoginActivity.java
+++ app/src/main/java/com/polimi/deib/ildiariodigio/LoginActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 public class LoginActivity extends AppCompatActivity {
 
     ImageButton parent_button;
+    ImageButton modifica_button; // dejo el enter por probar luego cuando este suelto habra que poner el lapiz
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +18,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         parent_button = (ImageButton)findViewById(R.id.imageButton_parent);
+        modifica_button = (ImageButton)findViewById(R.id.button_enter);
+
         parent_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Entering the menu", Toast.LENGTH_LONG).show();
                 Intent myIntent = new Intent(LoginActivity.this, HomeActivity.class);
+                LoginActivity.this.startActivity(myIntent);
+            }
+        });
+        modifica_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Entering the modify menu", Toast.LENGTH_LONG).show();
+                Intent myIntent = new Intent(LoginActivity.this, ModificaActivity.class);
                 LoginActivity.this.startActivity(myIntent);
             }
         });
--- app/src/main/java/com/polimi/deib/ildiariodigio/StartActivity.java
+++ app/src/main/java/com/polimi/deib/ildiariodigio/StartActivity.java
@@ -1,6 +1,8 @@
 package com.polimi.deib.ildiariodigio;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,19 +13,37 @@ import android.widget.Toast;
 public class StartActivity extends AppCompatActivity {
 
     ImageButton enter_button;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
         enter_button = (ImageButton)findViewById(R.id.imageButton_enter);
         enter_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Entering the application", Toast.LENGTH_LONG).show();
-                Intent myIntent = new Intent(StartActivity.this, LoginActivity.class);
-                StartActivity.this.startActivity(myIntent);
+                boolean previouslyStarted = prefs.getBoolean("pref_previously_started", false);
+                if(!previouslyStarted) {
+                    SharedPreferences.Editor edit = prefs.edit();
+                    edit.putBoolean("pref_previously_started", Boolean.TRUE);
+                    edit.commit();
+                    Toast.makeText(getApplicationContext(), "Entering the application", Toast.LENGTH_LONG).show();
+                    Intent myIntent = new Intent(StartActivity.this, FirstLoginActivity.class);
+                    StartActivity.this.startActivity(myIntent);
+
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Entering the application", Toast.LENGTH_LONG).show();
+                    Intent myIntent = new Intent(StartActivity.this, LoginActivity.class);
+                    StartActivity.this.startActivity(myIntent);
+                }
+
+
             }
         });
     }
