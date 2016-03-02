3e56549861ea845ece5142b28aa1bb839f8b2971
Diego <dialgu_zgz@hotmail.com>
2016-03-02 03:21:22 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_chronometer_audio.xml

--- app/src/main/java/com/polimi/deib/ildiariodigio/ChronometerMenuActivity.java
+++ app/src/main/java/com/polimi/deib/ildiariodigio/ChronometerMenuActivity.java
@@ -7,14 +7,13 @@ import android.view.View;
 import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.widget.Button;
 
 public class ChronometerMenuActivity extends AppCompatActivity {
 
-    ImageButton button_cronometro;
-    ImageButton button_agenda;
-    ImageButton button_ricorda;
-    ImageButton button_attivita;
-    ImageButton button_diario;
+    Button audio;
+    Button animation;
+    Button video;
     ImageButton button_back;
 
     @Override
@@ -22,55 +21,35 @@ public class ChronometerMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chronometer_menu);
 
-        /*button_animation = (ImageButton)findViewById(R.id.imageButton_animation);
-        button_agenda = (ImageButton)findViewById(R.id.imageButton_agenda);
-        button_ricorda = (ImageButton)findViewById(R.id.imageButton_ricorda);
-        button_attivita = (ImageButton)findViewById(R.id.imageButton_attivita);
-        button_diario = (ImageButton)findViewById(R.id.imageButton_diario);
+        audio = (Button)findViewById(R.id.button_audio);
+        animation = (Button)findViewById(R.id.button_animation);
+        video = (Button)findViewById(R.id.button_video);
         button_back = (ImageButton)findViewById(R.id.imageButton_back);
 
-        button_cronometro.setOnClickListener(new View.OnClickListener() {
+        audio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Cronometro", Toast.LENGTH_LONG).show();
-                Intent myIntent = new Intent(HomeActivity.this, AudioGridActivity.class);
-                HomeActivity.this.startActivity(myIntent);
+                Intent myIntent = new Intent(ChronometerMenuActivity.this, ChronometerAudioActivity.class);
+                ChronometerMenuActivity.this.startActivity(myIntent);
             }
         });
 
-        button_agenda.setOnClickListener(new View.OnClickListener() {
+        animation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Agenda", Toast.LENGTH_LONG).show();
-                Intent myIntent = new Intent(HomeActivity.this, HomeActivity.class);
-                HomeActivity.this.startActivity(myIntent);
+                Intent myIntent = new Intent(ChronometerMenuActivity.this, ChronometerAnimationActivity.class);
+                ChronometerMenuActivity.this.startActivity(myIntent);
             }
         });
 
-        button_ricorda.setOnClickListener(new View.OnClickListener() {
+        video.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Ricorda", Toast.LENGTH_LONG).show();
-                Intent myIntent = new Intent(HomeActivity.this, HomeActivity.class);
-                HomeActivity.this.startActivity(myIntent);
-            }
-        });
-
-        button_attivita.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Attivita", Toast.LENGTH_LONG).show();
-                Intent myIntent = new Intent(HomeActivity.this, HomeActivity.class);
-                HomeActivity.this.startActivity(myIntent);
-            }
-        });
-
-        button_diario.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Diario", Toast.LENGTH_LONG).show();
-                Intent myIntent = new Intent(HomeActivity.this, HomeActivity.class);
-                HomeActivity.this.startActivity(myIntent);
+                Intent myIntent = new Intent(ChronometerMenuActivity.this, ChronometerVideoActivity.class);
+                ChronometerMenuActivity.this.startActivity(myIntent);
             }
         });
 
@@ -81,6 +60,6 @@ public class ChronometerMenuActivity extends AppCompatActivity {
                 Intent myIntent = new Intent(ChronometerMenuActivity.this, HomeActivity.class);
                 ChronometerMenuActivity.this.startActivity(myIntent);
             }
-        });*/
+        });
     }
 }
