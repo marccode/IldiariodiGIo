cd9cd213867549634246ca851b2566f2615f28d1
Diego <dialgu_zgz@hotmail.com>
2016-03-04 05:06:03 +0100

    cambio en el titulo de activity_video_grid a i mei video (ponia i mei audio)
    he acortado los tamaños de las cajas del menu de cronometro
    
    los colores de los recuadros me aparecen en el android studio pero no en el emulador (no se como incluirlos de momento)
    
    accede a la camara pero aun no guarda

---
+++ app/src/main/java/com/polimi/deib/ildiariodigio/DiarioMenuActivity.java
@@ -0,0 +1,52 @@
+package com.polimi.deib.ildiariodigio;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class DiarioMenuActivity extends AppCompatActivity {
+
+    Button camera;
+    Button guarda_foto;
+    ImageButton back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diario_menu);
+
+        camera = (Button)findViewById(R.id.access_camera);
+        guarda_foto = (Button)findViewById(R.id.access_photos);
+        back = (ImageButton)findViewById(R.id.imageButton_back);
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DiarioMenuActivity.this, HomeActivity.class);
+                DiarioMenuActivity.this.startActivity(intent);
+            }
+        });
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
+        guarda_foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DiarioMenuActivity.this, DiarioGridActivity.class);
+                DiarioMenuActivity.this.startActivity(intent);
+            }
+        });
+
+    }
+}
