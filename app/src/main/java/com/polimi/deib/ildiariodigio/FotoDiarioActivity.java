0683806052541f0ade753c20652a4409206fce78
Diego <dialgu_zgz@hotmail.com>
2016-03-06 03:12:49 +0100

    hecho lo de las fotos, falta en la q se muestra y pequeños ajustes. lo subo para q no se pierda y asi

---
+++ app/src/main/java/com/polimi/deib/ildiariodigio/FotoDiarioActivity.java
@@ -0,0 +1,13 @@
+package com.polimi.deib.ildiariodigio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FotoDiarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foto_diario);
+    }
+}
--- app/src/main/java/com/polimi/deib/ildiariodigio/ModificaFotoActivity.java
+++ app/src/main/java/com/polimi/deib/ildiariodigio/ModificaFotoActivity.java
@@ -5,16 +5,29 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ModificaFotoActivity extends AppCompatActivity {
 
     ImageButton back;
+    ImageButton button_enter;
     boolean from_camera;
     private ImageView image;
+    DBAdapter db;
+    EditText et_title_photo;
+    EditText et_description_photo;
+    String title_photo;
+    String description_photo;
+    String date;
+    String file_name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +38,7 @@ public class ModificaFotoActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         from_camera = intent.getBooleanExtra("from_camera",true);
-        String file_name = intent.getStringExtra("path_name");
+        file_name = intent.getStringExtra("path_name");
 
         File imgFile = new  File(file_name);
         if(imgFile.exists())
@@ -50,5 +63,36 @@ public class ModificaFotoActivity extends AppCompatActivity {
 
             }
         });
+
+        et_title_photo = (EditText) findViewById(R.id.title_photo);
+        et_description_photo = (EditText) findViewById(R.id.description_photo);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+        date = dateFormat.format(new Date() );
+
+
+        db = new DBAdapter(getApplicationContext());
+        db.open();
+
+        button_enter = (ImageButton)findViewById(R.id.imageButton_enter);
+
+        button_enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                title_photo = et_title_photo.getText().toString();
+                description_photo = et_description_photo.getText().toString();
+
+                db.addPhoto(file_name, title_photo, description_photo, date);
+
+
+                db.close();
+
+                Toast.makeText(getApplicationContext(), "Entering menu", Toast.LENGTH_LONG).show();
+                Intent myIntent = new Intent(ModificaFotoActivity.this, FotoDiarioActivity.class);
+                ModificaFotoActivity.this.startActivity(myIntent);
+            }
+        });
+
     }
 }
